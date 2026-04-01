9a4e29fe30e82383a61a48c442c31f5478368bb2
JUNGMINLEE-7177 <min111700@naver.com>
2026-04-02 01:19:54 +0900

    20번 문제 추가 진도

--- src/ch06/problem/n20/BankApplication.java
+++ src/ch06/problem/n20/BankApplication.java
@@ -3,41 +3,50 @@ package ch06.problem.n20;
 import java.util.Scanner;
 
 public class BankApplication {
-	private static Account[] account = new Account[100];
+	private static Account[] accountArray = new Account[100];
+	private static Scanner scanner = new Scanner(System.in);
 	private int num = 0;
 	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
-		
-		
 		while(true) {
 			System.out.println("----------------------------------");
 			System.out.println("1.계좌생성|2.계좌목록|3.예금|4.출금|5.종료");
 			System.out.println("----------------------------------");
 			System.out.print("선택>");
+			
 			int select = scanner.nextInt();
 			System.out.println();
 			
 			switch (select) {
 				case 1 :
-					System.out.println("---------");
-					System.out.println("계좌생성");
-					System.out.println("---------");
-					System.out.print("계좌번호: ");
-					String accountNum = scanner.nextLine();
-					System.out.println();
-					System.out.print("계좌주: ");
-					String accountOwner = scanner.nextLine();
-					System.out.println();
-					System.out.print("초기금액: ");
-					int balance = scanner.nextInt();
-					System.out.println();
-					
-					account[num] = new Account();
-					account[num].setAccount(accountNum, accountOwner, balance);
-					
 			}
+
 			
 		}		
 	}
+	
+	//계좌생성
+	private static void createAccount(){
+		System.out.println("---------");
+		System.out.println("계좌생성");
+		System.out.println("---------");
+		
+		System.out.print("계좌번호: ");
+		String accountNum = scanner.nextLine();
+		System.out.println();
+		
+		System.out.print("계좌주: ");
+		String accountOwner = scanner.nextLine();
+		System.out.println();
+		
+		System.out.print("초기금액: ");
+		int balance = scanner.nextInt();
+		System.out.println();
+		
+		Account account = new Account(accountNum, accountOwner, balance);
+		
 
-}
+		
+		}
+	}
+
+
