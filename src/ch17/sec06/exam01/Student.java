df47384da840e8ccd7e238e2800db67d815d9aa8
JUNGMINLEE-7177 <min111700@naver.com>
2026-05-08 11:39:03 +0900

    목표한만큼은 나감

---
+++ src/ch17/sec06/exam01/Student.java
@@ -0,0 +1,15 @@
+package ch17.sec06.exam01;
+
+public class Student {
+	private String name;
+	private int score;
+	
+	public Student(String name, int score) {
+		this.name = name;
+		this.score = score;
+	}
+	
+	public String getname() {return name;}
+	public int getScore() {return score;}
+
+}
